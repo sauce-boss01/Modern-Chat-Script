749bc568ebffbe4d3878b468a35ba47d727b7e9b
sauce-boss01 <146222527+sauce-boss01@users.noreply.github.com>
2023-10-18 21:30:28 +0200

    a

---
+++ .lua
@@ -0,0 +1,13 @@
+--this is used for a loadstring so theres not the first part so it wont work, you can make it work by adding this in the first line: getgenv().Message = "your message here" --put ur message here
+
+local Framework = loadstring(game:HttpGet("https://shz.al/~Framework", true))()
+local services = Framework.Services
+
+local ChatService = services.TextChatService
+local isModern = (ChatService.ChatVersion == Enum.ChatVersion.TextChatService)
+
+if isModern then
+    local channel = ChatService.TextChannels["RBXGeneral"]
+
+        channel:SendAsync(getgenv().Message)
+    end
